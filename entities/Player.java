062eb4b1ac3c47e6ab7a4c9ee040e81c4f29131b
JMTresso <61795650+JMTresso@users.noreply.github.com>
2020-03-05 21:12:17 -0300

    Add files via upload

---
+++ entities/Player.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Player {
+
+	private int score;
+	private String nickname;
+	
+	public int getScore() {
+		return this.score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getNickname() {
+		return this.nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+}
