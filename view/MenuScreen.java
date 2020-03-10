29d44aaba9f2b9ea2b0c8c039bde84f827994893
Lucas Gabriel <48874910+LucasGabrielB@users.noreply.github.com>
2020-03-10 18:26:32 -0500

    teste

--- view/MenuScreen.java
+++ view/MenuScreen.java
@@ -1,14 +1,51 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import entities.Player;
 
 public class MenuScreen  extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
+	// screen size constants
+    public static final int WIDTH = 720, HEIGHT = 480;
+	
 	public MenuScreen(){
 		setFocusable(true);
+		setLayout(null); 
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        showScreen();
+        
 	}
+	
+	public void showScreen() {
+		JButton playButton = new JButton("Jogar");
+		JButton quitButton = new JButton("Sair");
+		playButton.setBounds(50, 100, 100, 40);
+		quitButton.setBounds(50, 150, 100, 40);
+		add(quitButton);
+		add(playButton);
+		
+		playButton.addActionListener(new ActionListener(){  
+			  public void actionPerformed(ActionEvent e){
+				  Player player = new Player("Rogerinho piroca de foice");
+				  GameScreen gameScreen = new GameScreen(player);
+				 
+				  add(gameScreen);           
+			  }
+		}); 
+		
+		quitButton.addActionListener(new ActionListener(){  
+			  public void actionPerformed(ActionEvent e){
+				  System.exit(0);  
+			  }
+		});
+		
+	}
+	
 }
