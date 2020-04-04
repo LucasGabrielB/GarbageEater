95c457432ef129b92922d196d563e20078a9a436
Lucas Gabriel <48874910+LucasGabrielB@users.noreply.github.com>
2020-04-04 14:34:42 -0300

    tela de avisos fecha ao apertar enter ou esc

--- view/WarningScreen.java
+++ view/WarningScreen.java
@@ -4,12 +4,15 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-public class WarningScreen extends JFrame{
+public class WarningScreen extends JFrame implements KeyListener{
 	private static final long serialVersionUID = 1L;
 	
 	// attributes
@@ -25,7 +28,8 @@ public class WarningScreen extends JFrame{
     	pack();
     	setLocationRelativeTo(null);
     	setVisible(true);
-    	setFocusable(true);	
+    	setFocusable(true);
+    	addKeyListener(this);
     	
     	// initializes all screen components
         initComponents();
@@ -53,6 +57,25 @@ public class WarningScreen extends JFrame{
 				  dispose(); 
 			  }
 		});
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		
+		if(key == KeyEvent.VK_ESCAPE || key == KeyEvent.VK_ENTER){
+			dispose();
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
 	}
 
 }
